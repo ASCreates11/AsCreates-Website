36c105527dd3fb202257c6d8cff5a52911427163
ASCreates <asish63710na@gmail.com>
2026-08-02 00:09:35 +0530

    Add PWA support for admin dashboard

---
+++ public/sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'ascreates-admin-v1';
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll([
+                '/admin/dashboard.html',
+                '/admin/login.html',
+                '/admin/admin.css',
+                '/favicon.png'
+            ]);
+        })
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
